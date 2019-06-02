aae5e0f8dfaabbd2a13103e40d9616686298f6af
Xiaoorange233 <51257469+Xiaoorange233@users.noreply.github.com>
2019-06-02 10:19:08 +0800

    Delete run-linux.sh

--- run-linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# 设置 Daemon 的运行端口
-port=21567
-# 设置 Daemon 的请求密码
-pass=123456789
-# 运行 Daemon
-echo "正在启动 PHPMC 7 Daemon"
-java -jar PHPMC7-Daemon.jar ${port} ${pass}
-echo "PHPMC 7 Daemon 已停止运行"
